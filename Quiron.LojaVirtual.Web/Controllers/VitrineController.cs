fe9edf02a3563d32ba25c82c24d2631b62eb12ba
rodrigoepereira <rodrigoepereira@gmail.com>
2015-04-04 16:01:22 -0300

    04/04/2015 COMMIT 2

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        //
+        // GET: /Vitrine/
+
+        private ProdutosRepositorio p;
+
+        private int ProdutoPorPagina = 3;
+
+        public ActionResult ListaProdutos( int pagina = 1)
+        {
+            p = new ProdutosRepositorio();
+            
+            var produto = p.todosProduto().OrderBy(x => x.Nome).Skip((pagina - 1) * ProdutoPorPagina).Take(ProdutoPorPagina);
+
+            return View(produto);
+        }
+	}
+}
